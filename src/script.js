ce3209e6b11c9c52a8789bc9b1d53d6c0ac6e585
dististik <austin4444@gmail.com>
2020-01-24 21:28:11 -0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+// This file is currently unnecessary as this chatbox doesn't use JavaScript to function at all.
+// It will be kept here as to not confuse people and in case KogaChat uses JavaScript at some point.
+// Commented below is the default code used in Streamlabs' chatbox.
+
+/*
 // Please use event listeners to run functions.
 document.addEventListener('onLoad', function(obj) {
 	// obj will be empty for chat widget
@@ -8,3 +13,4 @@ document.addEventListener('onEventReceived', function(obj) {
   	// obj will contain information about the event
 	
 });
+*/
