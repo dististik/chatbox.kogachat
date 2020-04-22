bf69bf894220f7a17a85088898459b0a1e68dae9
dististik <austin4444@gmail.com>
2020-04-21 21:59:12 -0700

    Prepend UTF-8 characterset to HTML header using jQuery

--- src/script.js
+++ src/script.js
@@ -1,2 +1,2 @@
-// This file is currently unnecessary as this chatbox doesn't use JavaScript to function at all.
-// It will be kept here as to not confuse people and in case KogaChat uses JavaScript at some point.
+// Set character encoding to UTF-8
+$('head').prepend('<meta charset="UTF-8"/>');
